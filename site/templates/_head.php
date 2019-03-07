0558f1e8f9e38c3b44e0c95512b43a66224ce34b
bbullemer <barbara.bullemer@gmail.com>
2019-03-07 13:57:49 -0600

    Update _head.php
    - added form for promo code
    - added error message

--- site/templates/_head.php
+++ site/templates/_head.php
@@ -20,7 +20,7 @@
     			<strong>Have any questions? We'd love to help.</strong> Give us a call at <?php echo $site->phone800; ?>
 			</div>
     	</div>
-    	
+
 		<div>
             <div class="container">
             	<br>
@@ -32,47 +32,88 @@
                     </div>
                     <div class="col-xs-3 col-sm-6">
                     	<div class="pull-right visible-xs-inline-block">
-                    		
+
                     			<a href="<?php echo $config->pages->account; ?>" class="navbar-brand account-link visible-xs-inline-block">
 									<span class="h3"><i class="glyphicon glyphicon-user" aria-hidden="true"></i></span>
 								</a>
-                    		
-                    		
+
+
                     	</div>
                     	<div class="login-navbar pull-right hidden-xs">
                         	<?php if ($user->loggedin) : ?>
                            		<h3>Welcome, <?php echo $user->fullname; ?></h3>
-                               	<div class="btn-group btn-group-sm" role="group" aria-label="Small button group"> 
-                               		<a href="<?php echo $config->pages->orders; ?>" class="btn btn-default"> 
-                               			<i class="glyphicon glyphicon-th-list " aria-hidden="true"></i> View My Orders 
+								<form action="<?php echo $config->pages->cart."redir/?action=promo"; ?>" method="POST" >
+									<input type="hidden" name="page" value="<?= $config->filename; ?>">
+									<div class="form-group">
+										<div class="input-group input-group-sm mb-3">
+									        <input type="text" class="form-control" name="promo" placeholder="Enter Promo Code Here">
+									        <span class="input-group-btn">
+									        	<button class="btn btn-sm btn-primary" type="submit">Submit</button>
+									        </span>
+									    </div>
+									</div>
+								</form>
+								<!-- TODO: fix error message with correct if statement  -->
+								<?php if ($user->loggedin) : ?>
+									<div class="alert alert-danger alert-dismissible" role="alert">
+									  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+									  <strong>Warning!</strong> Please enter a valid promo code
+									</div>
+								<?php else : ?>
+									<br>
+								<?php endif; ?>
+                               	<div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                               		<a href="<?php echo $config->pages->orders; ?>" class="btn btn-default">
+                               			<i class="glyphicon glyphicon-th-list " aria-hidden="true"></i> View My Orders
                                		</a>
-									<a href="<?php echo $config->pages->account; ?>"  class="btn btn-default"> 
-										<i class="glyphicon glyphicon-user" aria-hidden="true"></i> My Account 
+									<a href="<?php echo $config->pages->account; ?>"  class="btn btn-default">
+										<i class="glyphicon glyphicon-user" aria-hidden="true"></i> My Account
 									</a>
 									<a href="<?php echo $config->pages->account."redir/?action=logout"; ?>" class="btn btn-danger">
-										<i class="material-icons md-18">&#xE879;</i> Logout 
+										<i class="material-icons md-18">&#xE879;</i> Logout
 									</a>
                                	</div>
-                                
+
                             <?php else : ?>
 	                     		<h3>Welcome To Northwest Graphic</h3>
-	                     		<div class="btn-group btn-group-sm" role="group" aria-label="Small button group"> 
-                               		<a href="<?php echo $config->pages->orders; ?>" class="btn btn-default"> 
-                               			<i class="glyphicon glyphicon-th-list " aria-hidden="true"></i> View My Orders 
+								Have a Promo Code?
+								<form action="<?php echo $config->pages->cart."redir/?action=promo"; ?>" method="POST" >
+									<input type="hidden" name="page" value="<?= $config->filename; ?>">
+									<div class="form-group">
+										<div class="input-group input-group-sm mb-3">
+									        <input type="text" class="form-control" name="promo" placeholder="Enter Promo Code Here">
+									        <span class="input-group-btn">
+									        	<button class="btn btn-sm btn-primary" type="submit">Submit</button>
+									        </span>
+									    </div>
+									</div>
+								</form>
+								<!-- TODO: fix error message with correct if statement  -->
+								<?php if ($user->loggedin) : ?>
+									<div class="alert alert-danger alert-dismissible" role="alert">
+									  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+									  <strong>Warning!</strong> Please enter a valid promo code
+									</div>
+								<?php else : ?>
+									<br>
+								<?php endif; ?>
+	                     		<div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                               		<a href="<?php echo $config->pages->orders; ?>" class="btn btn-default">
+                               			<i class="glyphicon glyphicon-th-list " aria-hidden="true"></i> View My Orders
                                		</a>
-									<a href="<?php echo $config->pages->account; ?>"  class="btn btn-default"> 
-										<i class="glyphicon glyphicon-user" aria-hidden="true"></i> My Account 
+									<a href="<?php echo $config->pages->account; ?>"  class="btn btn-default">
+										<i class="glyphicon glyphicon-user" aria-hidden="true"></i> My Account
 									</a>
-									<a href="<?php echo $config->pages->login; ?>" class="btn btn-success btn-sm"> 
-										<i class="material-icons md-18">&#xE890;</i> Login 
+									<a href="<?php echo $config->pages->login; ?>" class="btn btn-success btn-sm">
+										<i class="material-icons md-18">&#xE890;</i> Login
 									</a>
                                	</div>
-                                
+
                         	<?php endif; ?>
                         </div>
                 	</div>
             	</div><br>
             </div>
         	<?php include ($config->paths->content.'nav/nav.php'); ?>
-           
+
          </div>
