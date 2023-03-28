ec5e2bae82db75335f3a1c8b2b2bbd8f8bf15b24
Paul <pauldro952@outlook.com>
2023-03-28 15:12:35 -0500

    Update billing-logic.php

--- site/templates/content/billing/billing-logic.php
+++ site/templates/content/billing/billing-logic.php
@@ -1,1 +1,6 @@
 <?php $billing = get_billing_information(session_id()); ?>
+<?php
+	if ($billing['paymenttype'] != 'bill' && $billing['cc'] == '' && $billing['termtype'] == 'STD') {
+		$billing['paymenttype'] = 'bill';
+	}
+?>
